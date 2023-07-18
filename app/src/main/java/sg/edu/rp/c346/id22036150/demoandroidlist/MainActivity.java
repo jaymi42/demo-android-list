3a62437b33baeb056c8f4fe42bf9a35d6a5ae992
jaymi <22036150@myrp.edu.sg>
2023-07-18 16:39:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22036150/demoandroidlist/MainActivity.java
@@ -0,0 +1,46 @@
+package sg.edu.rp.c346.id22036150.demoandroidlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvVersions;
+    ArrayList<AndroidVersion> alVersions;
+    //ArrayAdapter<AndroidVersion> aaVersions;
+    CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvVersions = findViewById(R.id.lvAndroidVersions);
+        alVersions = new ArrayList<>();
+
+
+        AndroidVersion item1 = new AndroidVersion("Pie", "9.0");
+        AndroidVersion item2 = new AndroidVersion("Oreo", "8.0 - 8.1");
+        AndroidVersion item3 = new AndroidVersion("Nougat", "7.0 - 7.1.2");
+
+        alVersions.add(item1);
+        alVersions.add(item2);
+        alVersions.add(item3);
+
+
+        //aaVersions = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alVersions);
+
+        adapter  = new CustomAdapter(this, R.layout.row, alVersions);
+
+        //lvVersions.setAdapter(aaVersions);
+
+        lvVersions.setAdapter(adapter);
+
+
+    }
+}
